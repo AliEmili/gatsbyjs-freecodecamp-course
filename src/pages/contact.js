6b93be44c64aa1240fe29d2c43e3c831c7ecadbc
aliemili <aliemili2013@yahoo.com>
2021-04-01 18:29:23 +0430

    create pages

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <h2>The best way to reach me is via @aliemili on Telegram.</h2>
+    </div>
+  )
+}
+
+export default Contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I'm Ali, a full-stack developer living in Mashhad.</h2>
+    </div>
+  )
 }
+
+export default Index
