4d704b6590758fde90f4478e6c378f40861c1639
aliemili <aliemili2013@yahoo.com>
2021-04-01 19:41:54 +0430

    create header component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Footer from "../components/footer"
+import Header from "../components/header"
 
 const Contact = () => {
   return (
     <div>
+      <Header />
       <h1>Contact</h1>
       <h2>
         The best way to reach me is via{" "}
