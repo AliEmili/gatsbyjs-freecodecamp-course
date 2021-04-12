748cbad59051e2bf86f464f7425f8fde0a4ce1b3
aliemili <aliemili2013@yahoo.com>
2021-04-12 23:00:48 +0430

    add some stylings

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 
+import * as blogStyles from "./blog.module.scss"
 const Blog = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -25,10 +26,10 @@ const Blog = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge, index) => {
           return (
-            <li key={index}>
+            <li className={blogStyles.post} key={index}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
